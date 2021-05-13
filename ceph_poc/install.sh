de3e8c733fe859aba1f8dd2e355002bf637d76fe
Zhihao Tang <zhihao.tang@futurewei.com>
2021-05-13 17:20:04 -0400

    Add MIT license to files

--- ceph_poc/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pushd /devhub/fksdr
-./velero.bin.v1 install --image ljtbbt/velero:v1 --provider aws --plugins velero/velero-plugin-for-aws:v1.0.0 --bucket velero --secret-file ./credentials-minio --use-volume-snapshots=false --use-restic --backup-location-config region=minio,s3ForcePathStyle=true,s3Url=http://10.124.48.118:9000,publicUrl=http://10.124.48.118:9000 2>&1 >/dev/null
-popd
